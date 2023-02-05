b7cfdcbb53e4368b92f4defba968138a7847b2f6
Longfeng Qin <sancaros@msn.cn>
2023-02-06 02:25:53 +0800

    更改任务日志

--- 舰船服务器/quests.c
+++ 舰船服务器/quests.c
@@ -189,8 +189,8 @@ static uint8_t *read_and_dec_dat(const char *fn, uint32_t *osz) {
 
     /* Read the file in. */
     if(!(fp = fopen(fn, "rb"))) {
-        QERR_LOG("无法打开任务文件 \"%s\"\n错误信息: %s", fn,
-              strerror(errno));
+        QERR_LOG("无法打开任务文件 \"%s\" ", fn);
+        QERR_LOG("错误信息: %s", strerror(errno));
         return NULL;
     }
 
@@ -199,14 +199,15 @@ static uint8_t *read_and_dec_dat(const char *fn, uint32_t *osz) {
     fseek(fp, 0, SEEK_SET);
 
     if(!(buf = (uint8_t *)malloc(sz))) {
-        QERR_LOG("无法分配内存去读取 dat\n错误信息: %s",
-              strerror(errno));
+        QERR_LOG("无法分配内存去读取 dat");
+        QERR_LOG("错误信息: %s", strerror(errno));
         fclose(fp);
         return NULL;
     }
 
     if(fread(buf, 1, sz, fp) != sz) {
-        QERR_LOG("无法读取 dat\n错误信息: %s", strerror(errno));
+        QERR_LOG("无法读取 dat");
+        QERR_LOG("错误信息: %s", strerror(errno));
         free_safe(buf);
         fclose(fp);
         return NULL;
