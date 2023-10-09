1c7c4837244d0f475b57ee6c0230bb71c4b4cbd7
Sancaros <sancaros@msn.cn>
2023-10-09 23:25:00 +0800

    修正物品属性描述 空值

--- 游戏库/libpsocn/pso_items.c
+++ 游戏库/libpsocn/pso_items.c
@@ -207,7 +207,7 @@ uint32_t get_item_amount(item_t* item, uint32_t amount) {
 		else if (amount > (uint8_t)max_stack_size(item)) {
 			item->datab[5] = (uint8_t)max_stack_size(item);
 		}
-		else if (amount > 0){
+		else if (amount > 0) {
 			item->datab[5] = (uint8_t)amount;
 		}
 	}
@@ -286,8 +286,8 @@ const char* bbitem_get_name_by_code(const item_t* item, int languageCheck) {
 	/* Look through the list for the one we want */
 	while (cur->code != BBItem_NoSuchItem) {
 		if (cur->datab[0] == item->datab[0]) {
-			if(item->datab[0] == ITEM_TYPE_TOOL && item->datab[1] == ITEM_SUBTYPE_DISK)
-				if(cur->datab[2] == item->datab[4])
+			if (item->datab[0] == ITEM_TYPE_TOOL && item->datab[1] == ITEM_SUBTYPE_DISK)
+				if (cur->datab[2] == item->datab[4])
 					return cur->name;
 
 
@@ -312,47 +312,47 @@ const char* bbitem_get_name_by_code(const item_t* item, int languageCheck) {
 
 /* 获取物品名称 */
 const char* item_get_name(const item_t* item, int version, int languageCheck) {
-//	uint32_t code = item->datab[0] | (item->datab[1] << 8) |
-//		(item->datab[2] << 16);
-//
-//	/* 获取相关物品参数对比 */
-//	switch (item->datab[0]) {
-//	case ITEM_TYPE_WEAPON:  /* 武器 */
-//		if (item->datab[5]) {
-//			code = (item->datab[5] << 8);
-//		}
-//		break;
-//
-//	case ITEM_TYPE_GUARD:  /* 装甲 */
-//		if (item->datab[1] != ITEM_SUBTYPE_UNIT && item->datab[3]) {
-//			code = code | (item->datab[3] << 16);
-//		}
-//		//printf("数据1: %02X 数据3: %02X\n", item->item_data1[1], item->item_data1[3]);
-//		break;
-//
-//	case ITEM_TYPE_MAG:  /* 玛古 */
-//		if (item->datab[1] == 0x00 && item->datab[2] >= 0xC9) {
-//			code = 0x02 | (((item->datab[2] - 0xC9) + 0x2C) << 8);
-//		}
-//		break;
-//
-//	case ITEM_TYPE_TOOL: /* 物品 */
-//		if (code == 0x060D03 && item->datab[3]) {
-//			code = 0x000E03 | ((item->datab[3] - 1) << 16);
-//		}
-//		break;
-//
-//	case ITEM_TYPE_MESETA: /* 美赛塔 */
-//		break;
-//
-//#ifdef DEBUG
-//	default:
-//		ERR_LOG("未找到物品类型");
-//		break;
-//
-//#endif // DEBUG
-//
-//	}
+	//	uint32_t code = item->datab[0] | (item->datab[1] << 8) |
+	//		(item->datab[2] << 16);
+	//
+	//	/* 获取相关物品参数对比 */
+	//	switch (item->datab[0]) {
+	//	case ITEM_TYPE_WEAPON:  /* 武器 */
+	//		if (item->datab[5]) {
+	//			code = (item->datab[5] << 8);
+	//		}
+	//		break;
+	//
+	//	case ITEM_TYPE_GUARD:  /* 装甲 */
+	//		if (item->datab[1] != ITEM_SUBTYPE_UNIT && item->datab[3]) {
+	//			code = code | (item->datab[3] << 16);
+	//		}
+	//		//printf("数据1: %02X 数据3: %02X\n", item->item_data1[1], item->item_data1[3]);
+	//		break;
+	//
+	//	case ITEM_TYPE_MAG:  /* 玛古 */
+	//		if (item->datab[1] == 0x00 && item->datab[2] >= 0xC9) {
+	//			code = 0x02 | (((item->datab[2] - 0xC9) + 0x2C) << 8);
+	//		}
+	//		break;
+	//
+	//	case ITEM_TYPE_TOOL: /* 物品 */
+	//		if (code == 0x060D03 && item->datab[3]) {
+	//			code = 0x000E03 | ((item->datab[3] - 1) << 16);
+	//		}
+	//		break;
+	//
+	//	case ITEM_TYPE_MESETA: /* 美赛塔 */
+	//		break;
+	//
+	//#ifdef DEBUG
+	//	default:
+	//		ERR_LOG("未找到物品类型");
+	//		break;
+	//
+	//#endif // DEBUG
+	//
+	//	}
 
 	if (version == 5)
 		return bbitem_get_name_by_code(item, languageCheck);
@@ -430,28 +430,28 @@ const char* get_weapon_special_describe(uint8_t value, int lang) {
 	int len_weapon = ARRAYSIZE(weapon_specials);
 	for (int i = 0; i < len_weapon; i++) {
 		if (weapon_specials[i].id == value) {
-			if(!lang)
+			if (!lang)
 				return weapon_specials[i].cn_name;
 			else
 				return weapon_specials[i].name;
 
 		}
 	}
-	return "";
+	return "无效EX";
 }
 
 const char* get_s_rank_special_describe(uint8_t value, int lang) {
 	int len_s_rank = ARRAYSIZE(s_rank_specials);
 	for (int i = 0; i < len_s_rank; i++) {
 		if (s_rank_specials[i].id == value) {
-			if(!lang)
+			if (!lang)
 				return s_rank_specials[i].cn_name;
 			else
 				return s_rank_specials[i].name;
 
 		}
 	}
-	return "";
+	return "无效EX";
 }
 
 const char* get_unit_bonus_describe(const item_t* item) {
@@ -459,7 +459,7 @@ const char* get_unit_bonus_describe(const item_t* item) {
 
 	memset(item_attrib_des, 0, sizeof(item_attrib_des));
 
-	char attrib1[50] = { 0 };
+	char attrib1[50] = "空";
 
 	for (x; x < ARRAYSIZE(unit_attrib_val); x++) {
 		if (item->datab[6] == unit_attrib_val[x]) {
@@ -484,30 +484,23 @@ char* get_weapon_attrib_describe(const item_t* item) {
 
 	memset(item_attrib_des, 0, sizeof(item_attrib_des));
 
-	char attrib1[50] = { 0 };
-	char attrib2[50] = { 0 };
-	char attrib3[50] = { 0 };
+	char attrib1[50] = "空";
+	char attrib2[50] = "空";
+	char attrib3[50] = "空";
 
 	// 处理属性1
 	if (item->datab[6] < attrib_len)
 		sprintf(attrib1, "%s%d", weapon_attrib[item->datab[6]], item->datab[7]);
-	else
-		strcpy(attrib1, "空");
 
 	// 处理属性2
 	if (item->datab[8] < attrib_len)
 		sprintf(attrib2, "%s%d", weapon_attrib[item->datab[8]], item->datab[9]);
-	else
-		strcpy(attrib2, "空");
 
 	// 处理属性3
 	if (item->datab[10] < attrib_len)
 		sprintf(attrib3, "%s%d", weapon_attrib[item->datab[10]], item->datab[11]);
-	else if(item->datab[10] & 0x80){
+	else if (item->datab[10] & 0x80) {
 		sprintf(attrib3, "解封%d", item->datab[11]);
-	}
-	else {
-		strcpy(attrib3, "空");
 	}
 
 	sprintf(item_attrib_des, "[%s/%s/%s]", attrib1, attrib2, attrib3);
