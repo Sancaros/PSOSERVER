a204b2dca080afb0902b59453859c8fc111defdd
Sancaros <sancaros@msn.cn>
2023-10-15 15:49:55 +0800

    新增舰闸通信发送数据DEBUG

--- 舰船服务器/shipgate.c
+++ 舰船服务器/shipgate.c
@@ -175,6 +175,11 @@ static int send_raw(shipgate_conn_t* c, int len, uint8_t* sendbuf, int crypt) {
             return 0;
         }
 
+        shipgate_hdr_t* pkt = (shipgate_hdr_t*)sendbuf;
+
+        DBG_LOG("shipgate_conn_t send_raw \ntype:0x%04X \nlen:0x%04X \nversion:0x%02X \nreserved:0x%02X \nflags:0x%04X"
+            , ntohs(pkt->pkt_type), ntohs(pkt->pkt_len), pkt->version, pkt->reserved, pkt->flags);
+
         pthread_rwlock_wrlock(&c->rwlock);
         //print_ascii_hex(dbgl, sendbuf, len);
 
